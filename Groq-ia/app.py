95139c098472f6fdaa434e98633b9b1cdb040cb1
Vinicius Franck Lourenço <franck.vinicius7@hotmail.com>
2026-05-30 16:17:37 -0300

    Atualização 2.4

--- Groq-ia/app.py
+++ Groq-ia/app.py
@@ -34,7 +34,8 @@ st.set_page_config(
 
 #login Google para usar IA
 if not st.user.is_logged_in:
-    st.button("Entrar com Google", on_clickst=lambda: st.login("google"))
+    if st.button("Entrar com Google"):
+        st.login("google")
     st.stop()
 
 st.sidebar.success(f"Logado como {st.user.email}")
